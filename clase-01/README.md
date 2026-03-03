5d2aba3665546a23313088c691a6eb56313e272d
Vincenzo Acconcia <vincenzoacconcia@MacBook-Pro-di-Vincenzo.local>
2026-03-03 16:05:16 +0100

    primer servidor

---
+++ clase-01/README.md
@@ -0,0 +1,44 @@
+
+
+# Api Tienda
+
+
+## Base url
+
+http://localhost:3000
+
+
+### Endpoints
+
+
+#### GET / ping
+
+
+Verifica que la API esta funcionando.
+
+
+
+##### GET / products
+````json
+{
+    "id": 1,
+    "name": "laptop",
+    "price": 1200
+  }
+````
+
+
+
+
+##### Response 200
+
+
+```json
+{
+  "id": 1,
+  "name": "electronics"
+}
+````
+
+
+
--- clase-01/index.js
+++ clase-01/index.js
@@ -3,6 +3,18 @@
 
 import * as http from "node:http";
 
+
+const products =[
+    {id: 1, name: "laptop", price: 1200},
+    {id: 2, name: "mouse", price: 20}
+    ];
+
+
+const categories = [
+    {id: 1, name: "electronics"},
+    {id: 2, name: "accessories"}
+];
+
 const server = http.createServer((req, res) =>{
     console.log(req.url)
     
@@ -19,19 +31,61 @@ const server = http.createServer((req, res) =>{
     res.end(JSON.stringify({message: "pong"}));
     return;
    }
+  
+// Productos
     if(req.url == "/products"){
     res.statusCode = 200;
     res.setHeader("Content-type", "application/json");
-    res.end(JSON.stringify({message: "lista de productos"}));
+    res.end(JSON.stringify(products));
     return;
    }
-   
+   if(req.url.startsWith("/products/")){
+   const id = Number (req.url.split("/")[2]);
+   const product = products.find( p => p.id == id);
+
+   if (!product){
     res.statusCode = 404;
+    res.setHeader("Content-type", "application/json");
+    res.end(JSON.stringify({message: "producto no encontrado"}));
+    return;
+   }
+    res.statusCode = 200;
+    res.setHeader("Content-type", "application/json");
+    res.end(JSON.stringify(product));
+    return;
+  
+   };
+ // Categories
+   if (req.url == "/categories"){
+    res.statusCode = 200;
+    res.setHeader("Content-type", "application/json");
+    res.end(JSON.stringify(categories));
+   }
+
+  
+  if(req.url.startsWith("/categories/")){
+   const id = Number (req.url.split("/")[2]);
+   const category = categories.find( (cat) => cat.id == id);
+
+   if (!category){
+    res.statusCode = 404;
+    res.setHeader("Content-type", "application/json");
+    res.end(JSON.stringify({message: "categoria no encontrada"}));
+    return;
+   }
+    res.statusCode = 200;
+    res.setHeader("Content-type", "application/json");
+    res.end(JSON.stringify(category));
+    return;
+  
+   };
+   
+   res.statusCode = 404;
     res.setHeader("Content-type", "application/json");
     res.end(JSON.stringify({message: "Not found"}));
     
 });
 
 server.listen(3000, ()=>{
-    console.log("http://localhost:3000")
+    console.log("http://localhost:3000");
 });
