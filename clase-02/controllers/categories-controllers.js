194abcb20a16d5fed75e12a90bbe8afde6f7b2c6
Vincenzo Acconcia <vincenzoacconcia@MacBook-Pro-di-Vincenzo.local>
2026-03-05 20:18:46 +0100

    updateCategory

--- clase-02/controllers/categories-controllers.js
+++ clase-02/controllers/categories-controllers.js
@@ -50,20 +50,26 @@ const category = categories.find((cat)=> cat.id == id);
     res.status(201).json(newCategory);
  };
 
-export const updateCategory = (req, res) => {
-  const id = parseInt(req.params.id);
-  if (isNaN(id)) {
-    return res.status(400).json({ error: "invalid category id" });
+
+
+export const updateCategory = (req, res)=>{
+  const id = Number(req.params.id)
+  if(isNaN(id)){
+    return res.status(400).json({error: "invalid id"})
+}
+const category = categories.find((cat)=>{cat.id == id});
+if(!category){
+    return res.status(404).json({error: "category not found"})
   }
-  const category = categories.find((cat) => cat.id === id);
-  if (!category) {
-    return res.status(404).json({ error: "category not found" });
-  }
-  if (req.body.name !== undefined) {
-    category.name = req.body.name;
-  }
-  if (req.body.description !== undefined) {
-    category.description = req.body.description;
-  }
-  res.json(category);
-};
+
+  console.log(product);
+  console.log(req.body);
+
+
+  const {name, description} = req.body
+
+  category.name =name
+  category.description = description;
+
+  req.json(category);
+ }
